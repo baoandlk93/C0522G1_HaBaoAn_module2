d958c91601ee46b417e4acd83d99c1a9a509b3e9
habaoan93 <baoandlk93@gmail.com>
2022-07-13 20:20:56 +0700

    ok

--- src/ss10_dsa_list/excercise/method_arraylist/service/impl/MyListService.java
+++ src/ss10_dsa_list/excercise/method_arraylist/service/impl/MyListService.java
@@ -8,6 +8,13 @@ public class MyListService<E> {
     public Object[] elements;
 
 
+    public MyListService(){
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyListService(int capacity){
+        elements = new Object[capacity];
+    }
     public boolean add(E e) {
         elements[size] = e;
         size++;
